52c86263ff3ffb132213914d69d8075dcb158206
Paul Meinz <paulmeinz@gmail.com>
2015-12-10 08:21:40 -0800

    refactor: change the query_data documentation feat: add additional data validation for unsupported columns in query_data

--- R/query_data.R
+++ R/query_data.R
@@ -1,27 +1,88 @@
+#' Query data for standard data packet
+#'
+#' A function for querying demographic data for standard college
+#' data packets. Returns an object with class data.frame and lrdataset
+#'
+#' @param dsn A data source name for the archive side of the LRDB. See the
+#' readme on how to set up a data source name.
+#' @param query A sql query in the form of a character vector or length one
+#' the query must pull student id, term (e.g, 1139), subject (e.g., 'PSYC'),
+#' and long subject ('Psychology')
+#' @param demo_names A character vector of column names corresponding with
+#' the order of columns in your sql query. The column names must be from
+#' a standard list corresponding with currently supported data (see details)
+#' and must contain 'id','term','subject', and 'subject_long'
+#'
+#' @details
+#' In order for this function to yield the necessary data, the user
+#' must pass a sql query (param query) for the archive LRDB and a vector of
+#' column names (param demo_names) corresponding with the columns of the query.
+#' For example, if the first column of the query selected student id, then the
+#' first column name provided would be 'id'. The function will only support
+#' certain types of query data (e.g., student id, age, etc.), each of these
+#' data types has a standard column name that must be supplied. Each column name
+#' is listed below with associated query data.
+#'
+#' 'id' - student id
+#'
+#' 'term' - 4-digit term identifier
+#'
+#' 'subject' - 2-5 letter course prefix (e.g., 'PSYC')
+#'
+#' 'long_subject' - full subject description (e.g, 'Psychology')
+#'
+#' 'course_number' - course number, up to 3 digits (e.g., 300)
+#'
+#' 'age' - student age during a given term
+#'
+#' 'matr_goal' - matriculation goal description (e.g.'Transfer to a 4-year')
+#'
+#' 'ed_level' - education level description (e.g., 'Completed GED')
+#'
+#' 'ethnicity' - student race
+#'
+#' 'inst_mode' - instructional mode description (e.g., 'lecture')
+#'
+#' 'language' - student's primary language
+#'
+#' 'gender' - student gender (m,f,u)
+#'
+#' 'grade' - official course grade (e.g., 'A','B','C', etc.)
 #'
 #'
+#' Do not modify or recode the data in any way (e.g., with a case when). The
+#' column names within the LRDB have not been provided here to keep the
+#' structure of the database secure. A standard query can be provided upon
+#' request. Contact \email{meinzp@@crc.losrios.edu}.
 #'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
+#' @examples
+#' \dontrun{
+#' query_data(dsn = 'datasourcename',
+#'            query = 'select id, term, subject, subject_descr...',
+#'            demo_names = c('id','term','subject','subject_long'))
+#'            }
 
-query_data <- function(connect_obj, query,
-                       demo_names = c('id','term','subject','subject_long',
-                                      'course_number','age','matr_goal',
-                                      'ed_level','ethnicity','enroll_status',
-                                      'inst_mode','language','gender',
+query_data <- function(dsn, query,
+                       demo_names = c('id', 'term', 'subject', 'subject_long',
+                                      'course_number', 'age', 'matr_goal',
+                                      'ed_level', 'ethnicity', 'enroll_status',
+                                      'inst_mode', 'language', 'gender',
                                       'grade')) {
 
   # Input validation
+  supported <- c('id','term', 'subject', 'subject_long', 'course_number', 'age',
+                 'matr_goal', 'ed_level','ethnicity', 'enroll_status',
+                 'inst_mode', 'language', 'gender', 'grade')
   required <- c('id','term','subject','subject_long')
   test <- required %in% demo_names
 
+  # Note didn't use stopifnot because I wanted a custom error message
   if (FALSE %in% test) {
     stop(paste("demo_names must contain", required[!test], ";", sep = ' '))
+  }
+
+  if (FALSE %in% (demo_names %in% supported)) {
+    stop("non-standard column name provided")
   }
 
   # Make query lowercase
