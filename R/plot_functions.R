dd24ddf90fb0705bd17ab805b2576bedc2c46a3f
Paul Meinz <paulmeinz@gmail.com>
2015-12-15 11:42:07 -0800

    feat: add a function for plotting success rates

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -8,7 +8,11 @@
 #'
 
 
-disag_hc_plot <- function(data, demo_col) {
+disag_hc_plot <- function(data,
+                          demo_col,
+                          path,
+                          x_axis_lab = '',
+                          title = '') {
 
   # Determine if data object of correct class
   if (class(data)[2] != 'lrdataset') {
@@ -44,9 +48,6 @@ disag_hc_plot <- function(data, demo_col) {
   color_scheme <- set_colors(pt_data[, 'acad_year'])
   xaxis_loc <- set_xaxis_label_loc(pt_data[, 'demo_col'])
 
-  # Set title
-  title <- paste('default plot title')
-
   # Generate a plot
   plot <- ggplot(data = pt_data,
                  aes(x = demo_col,
@@ -63,7 +64,7 @@ disag_hc_plot <- function(data, demo_col) {
                     angle = 90, hjust = label_loc) +
           scale_fill_manual(values = color_scheme) +
           ggtitle(title)+
-          xlab("default label") +
+          xlab(x_axis_lab) +
           ylab("Percentage") +
           guides(fill=guide_legend(title="Academic Year")) +
           scale_y_continuous(limits = c(0,1))+
@@ -80,6 +81,72 @@ disag_hc_plot <- function(data, demo_col) {
 
 
   print(plot)
-  ggsave(paste('C:/Users/W1563070/Documents/testing.jpg'),
-         width=8.694, height=6.9583, dpi=72)
+  ggsave(path, width=8.694, height=6.9583, dpi=72)
+}
+
+
+disag_scs_plot <- function(data,
+                           demo_col,
+                           path,
+                           x_axis_lab = '',
+                           title = '') {
+
+  # Determine if data object of correct class
+  if (class(data)[2] != 'lrdataset') {
+    stop('This function only supports lrdataset data objects')
+  }
+
+  # Change the demo_column name to generic name
+  demo <- names(data) == demo_col
+  names(data)[demo] <- 'demo_col'
+
+  # Get average succes by year and demo
+  pt_data <- plyr::ddply(unique_data,
+                           c('demo_col', 'acad_year'),
+                           summarise,
+                           success = mean(success), .drop = F)
+
+  # Use utils to calculate plot features (data level location, color, xaxis
+  # label angle
+
+  label_loc <- set_label_loc(pt_data[, 'demo_col'],
+                             pt_data$success)
+  color_scheme <- set_colors(pt_data[, 'acad_year'])
+  xaxis_loc <- set_xaxis_label_loc(pt_data[, 'demo_col'])
+
+  # Generate a plot
+  plot <- ggplot(data = pt_data,
+                 aes(x = demo_col,
+                     y = success,
+                     fill = acad_year)) +
+    geom_bar(stat = 'identity', position = 'dodge') +
+    geom_text(aes(x = demo_col,
+                  y = success,
+                  ymax = success,
+                  label = paste(round(success*100
+                                      , digit = 1),
+                                '%', sep = '')),
+              position=position_dodge(width=.9), vjust = .4, size = 4.5,
+              angle = 90, hjust = label_loc) +
+    scale_fill_manual(values = color_scheme) +
+    ggtitle(title)+
+    xlab(x_axis_lab) +
+    ylab("Percentage") +
+    guides(fill=guide_legend(title="Academic Year")) +
+    scale_y_continuous(limits = c(0,1))+
+    theme(legend.position='bottom',
+          axis.text = element_text(size = 12),
+          axis.title = element_text (size = 12, face = 'bold'),
+          plot.title = element_text(size = 12, face = 'bold'),
+          legend.text = element_text(size = 9),
+          legend.title = element_text(size = 12),
+          axis.text.x = element_text(angle = xaxis_loc[1],
+                                     hjust = xaxis_loc[2]
+                                     , colour='black'),
+          axis.text.y=element_text(colour='black'))
+
+
+  print(plot)
+  ggsave(path, width=8.694, height=6.9583, dpi=72)
+
 }
