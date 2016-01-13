45a38eeae6541cb2904e5370b0ebbb70029235a8
Paul Meinz <paulmeinz@gmail.com>
2016-01-13 11:54:58 -0800

    feat: generate_packets produces age range headcount plot

--- R/generate_packets.R
+++ R/generate_packets.R
@@ -15,8 +15,15 @@ generate_packets <- function(data,
     for (i in subjects) {
       plot_data <- data[data$subject_long == i,]
       subject <- plot_data[1, 'subject']
-      plot_headcounts(plot_data, paste(path, subject, '.jpg'), undup = FALSE)
-      plot_headcounts(plot_data, paste(path, subject, '1.jpg'), undup = TRUE)
+      title <- paste(i,':','\n', 'Duplicated Headcount by Academic Term', sep = '')
+      save <- paste(path, subject,'.jpg', sep = '')
+      plot_headcounts(plot_data, save, title, undup = FALSE)
+      title <- paste(i,':','\n', 'Unuplicated Headcount by Academic Term', sep = '')
+      save <- paste(path, subject, '1.jpg', sep = '')
+      plot_headcounts(plot_data, save, title)
+      title <- paste(i,':','\n', 'Headcount by Age Range', sep = '')
+      save <- paste(path, subject, '2.jpg', sep = '')
+      disag_hc_plot(data, 'age', save, 'Age Range', title)
     }
   }
 }
