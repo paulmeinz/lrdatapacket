f154124b7fac7f6f6f00d55a15a65f5aca5381b8
Paul Meinz <paulmeinz@gmail.com>
2016-01-13 10:53:36 -0800

    feat: generate_packets now saves plots

--- R/generate_packets.R
+++ R/generate_packets.R
@@ -10,13 +10,11 @@ generate_packets <- function(data,
                              program = '') {
 
   subjects <- unique(data$subject_long)
-  print(subjects)
 
   if (use_subject) {
     for (i in subjects) {
       plot_data <- data[data$subject_long == i,]
-      subject <- plot_data[data$subject, 1]
-      print(subject)
+      subject <- plot_data[1, 'subject']
       plot_headcounts(plot_data, paste(path, subject, '.jpg'), undup = FALSE)
       plot_headcounts(plot_data, paste(path, subject, '1.jpg'), undup = TRUE)
     }
